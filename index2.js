9484928dcaca80796445ccecd32313829c6122a8
= <rahulkumar@metafic.co>
2023-06-17 20:20:00 +0530

    synchronous and asynchronous read and write

---
+++ index2.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+
+
+// add foler files with file f1.txt
+
+// how to read file in synchronous mode
+// function readFile() {
+//     try{
+//         const data = fs.readFileSync('./files/f1.txt', 'utf8');
+//         console.log('Synchronous Read: ',data);
+//     }catch(e){
+//         console.error('Error reading file', e);
+//     }
+// }
+
+// readFile();
+
+//How to read file asynchronously
+// fs.readFile('./files/f1.txt', 'utf8', (err, data) => {
+//     if(err) {
+//         console.error('Error reading file', err);
+//         return;
+//     }
+
+//     console.log('Synchronous Read successfully : ', data);
+    
+// });
+
+// how to write file synchronully
+const content = 'Hello My name is Rahul i am a developer'
+
+// function writeFile() {
+//     try{
+//        fs.writeFileSync('./files/f1.txt', content, 'utf8');
+        
+//     }catch(e){
+//         console.error('Error writing file', e);
+//     }
+// }
+
+// writeFile();
+
+// how to write file asynchronously
+
+fs.writeFile('./files/f1.txt', content, 'utf8', (err) => {
+    if(err) {
+        console.error('Error writing file', err);
+        return;
+    }
+    console.log('asynchronously wrote successfully');
+});
+
