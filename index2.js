65973e149b4e284ea301bb98de21385e73772424
= <rahulkumar@metafic.co>
2023-06-17 20:29:37 +0530

    synchronously and asynchronusly create directories

--- index2.js
+++ index2.js
@@ -42,11 +42,35 @@ const content = 'Hello My name is Rahul i am a developer'
 
 // how to write file asynchronously
 
-fs.writeFile('./files/f1.txt', content, 'utf8', (err) => {
+// fs.writeFile('./files/f1.txt', content, 'utf8', (err) => {
+//     if(err) {
+//         console.error('Error writing file', err);
+//         return;
+//     }
+//     console.log('asynchronously wrote successfully');
+// });
+
+
+// how to create directories synchronously
+
+// function createDirectory() {
+//     try{
+//         fs.mkdirSync('new-directory');
+//         console.log('synchronously created directory');
+//     }catch(e){
+//         console.error('error creating directory', e);
+//     }
+// }
+
+// createDirectory();
+
+// how to create directories asynchronously
+
+fs.mkdir('new-directory', (err) => {
     if(err) {
-        console.error('Error writing file', err);
+        console.error('error creating directory', err);
         return;
     }
-    console.log('asynchronously wrote successfully');
-});
 
+    console.log('synchronously created directory');
+});
